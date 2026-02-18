5008a7c28dc53f6845a105674d8cd517fecd68cc
betzaleltusk <betzaleltusk@gmail.com>
2026-02-18 17:09:47 +0200

    chore: Remove all project-related files and configurations.

--- test_and_ignore_this.js
+++
@@ -1,8 +0,0 @@
-let variable = 0;
-let sum = 0;
-for (let i = 0; i < 10; i++) {
-  variable = variable + 2;
-  console.log(variable + " variable " + i);
-  sum = sum + variable;
-  console.log(sum + " sum " + i);
-}
